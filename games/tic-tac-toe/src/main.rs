6faaa6da9b97e282ddfd823fa9e090338ce37673
Zachary Patten <sevenix.zp@gmail.com>
2021-12-11 19:35:09 -0500

    tic-tac-toe updates

--- games/tic-tac-toe/src/main.rs
+++ games/tic-tac-toe/src/main.rs
@@ -3,69 +3,70 @@ use rand::prelude::*;
 use std::io::prelude::*;
 use crossterm::event::*;
 
-static mut random : ThreadRng = rand::thread_rng();
 static mut closeRequested : bool = false;
 static mut playerTurn : bool = true;
 static mut board : [[char; 3]; 3] = [[' '; 3]; 3];
 
 fn main()
 {
-    while !closeRequested
+    unsafe
     {
-        board = [[' '; 3]; 3];
         while !closeRequested
         {
-            if playerTurn
+            board = [[' '; 3]; 3];
+            while !closeRequested
             {
-                PlayerTurn();
-                if CheckForThree('X')
+                if playerTurn
                 {
-                    EndGame("You Win.");
+                    PlayerTurn();
+                    if CheckForThree('X')
+                    {
+                        EndGame("You Win.");
+                        break;
+                    }
+                }
+                else
+                {
+                    ComputerTurn();
+                    if CheckForThree('O')
+                    {
+                        EndGame("You Lose.");
+                        break;
+                    }
+                }
+                playerTurn = !playerTurn;
+                if CheckForFullBoard()
+                {
+                    EndGame("Draw.");
                     break;
                 }
             }
-            else
+            if !closeRequested
             {
-                ComputerTurn();
-                if CheckForThree('O')
+                println!();
+                println!("Play Again [enter], or quit [escape]?");
+                let mut validInput = false;
+                while !closeRequested && !validInput
                 {
-                    EndGame("You Lose.");
-                    break;
+                    let event = read().unwrap();
+                    if event == Event::Key(KeyCode::Enter.into())
+                    {
+                        validInput = true;
+                    }
+                    if event == Event::Key(KeyCode::Esc.into())
+                    {
+                        closeRequested = true;
+                    }
                 }
-            }
-            playerTurn = !playerTurn;
-            if CheckForFullBoard()
-            {
-                EndGame("Draw.");
-                break;
-            }
-        }
-        if !closeRequested
-        {
-            println!();
-            println!("Play Again [enter], or quit [escape]?");
-        GetInput:
-            switch (Console.ReadKey(true).Key)
-            {
-                case ConsoleKey.Enter: break;
-                case ConsoleKey.Escape:
-                    closeRequested = true;
-                    Console.Clear();
-                    break;
-                default: goto GetInput;
             }
         }
     }
-
-    println!("tic-tac-toe");
-    println!();
-    println!("Still under development...");
 }
 
-fn PlayerTurn()
+unsafe fn PlayerTurn()
 {
-    let mut row : u32 = 0;
-    let mut column : u32 = 0;
+    let mut row    : usize = 0;
+    let mut column : usize = 0;
     let mut moved : bool = false;
     while !moved && !closeRequested
     {
@@ -74,30 +75,51 @@ fn PlayerTurn()
         println!();
         println!("Choose a valid position and press enter.");
         cursor(row * 4 + 1, column * 6 + 1);
-        switch (Console.ReadKey(true).Key)
+        let event = read().unwrap();
+
+        if event == Event::Key(KeyCode::Left.into()) 
         {
-            case ConsoleKey.UpArrow:    row = row <= 0 ? 2 : row - 1; break;
-            case ConsoleKey.DownArrow:  row = row >= 2 ? 0 : row + 1; break;
-            case ConsoleKey.LeftArrow:  column = column <= 0 ? 2 : column - 1; break;
-            case ConsoleKey.RightArrow: column = column >= 2 ? 0 : column + 1; break;
-            case ConsoleKey.Enter:
-                if (board[row, column] is ' ')
-                {
-                    board[row, column] = 'X';
-                    moved = true;
-                }
-                break;
-            case ConsoleKey.Escape:
-                Console.Clear();
-                closeRequested = true;
-                break;
+            if row == 0 { row = 2; }
+            else { row -= 1; }
         }
+        if event == Event::Key(KeyCode::Right.into())
+        {
+            if row == 2 { row = 0; }
+            else { row += 1; }
+        }
+        if event == Event::Key(KeyCode::Up.into())    
+        {
+            if column == 0 { column = 2;}
+            else { column -= 1; }
+        }
+        if event == Event::Key(KeyCode::Down.into())
+        {
+            if column == 2 { column = 0; }
+            else { column += 1; }
+        }
+        if event == Event::Key(KeyCode::Esc.into())
+        {
+            closeRequested = true;
+        }
+        if event == Event::Key(KeyCode::Enter.into())
+        {
+            if (board[row][column] == ' ')
+            {
+                board[row][column] = 'X';
+                moved = true;
+            }
+        }
+        if row    < 0 { row = 2; }
+        if row    > 2 { row = 0; }
+        if column < 0 { column = 2; }
+        if column > 2 { column = 0; }
     }
 }
 
-fn ComputerTurn()
+unsafe fn ComputerTurn()
 {
-    let possibleMoves: Vec<(usize, usize)> = Vec::new();
+    let mut random : ThreadRng = rand::thread_rng();
+    let mut possibleMoves: Vec<(usize, usize)> = Vec::new();
     for i in 0..3
     {
         for j in 0..3
@@ -113,7 +135,7 @@ fn ComputerTurn()
     board[x][y] = 'O';
 }
 
-fn CheckForThree(c: char) -> bool
+unsafe fn CheckForThree(c: char) -> bool
 {
     return
         board[0][0] == c && board[1][0] == c && board[2][0] == c ||
@@ -126,7 +148,7 @@ fn CheckForThree(c: char) -> bool
         board[2][0] == c && board[1][1] == c && board[0][2] == c;
 }
 
-fn CheckForFullBoard() -> bool
+unsafe fn CheckForFullBoard() -> bool
 {
     return
         board[0][0] != ' ' && board[1][0] != ' ' && board[2][0] != ' ' &&
@@ -134,7 +156,7 @@ fn CheckForFullBoard() -> bool
         board[0][2] != ' ' && board[1][2] != ' ' && board[2][2] != ' ';
 }
 
-fn RenderBoard()
+unsafe fn RenderBoard()
 {
     println!();
     println!(" {}  ║  {}  ║  {}", board[0][0], board[0][1], board[0][2]);
@@ -148,7 +170,7 @@ fn RenderBoard()
     println!(" {}  ║  {}  ║  {}", board[2][0], board[2][1], board[2][2]);
 }
 
-fn EndGame(message: &str)
+unsafe fn EndGame(message: &str)
 {
     clear();
     RenderBoard();
@@ -167,7 +189,7 @@ fn flush()
     match io::stdout().flush() { _ => {} };
 }
 
-fn cursor(row : u32, column : u32)
+fn cursor(row : usize, column : usize)
 {
     print!("\033[{};{}H", row, column);
 }
